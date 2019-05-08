55b013a96326f1fd01ca6f1dd5995abe233b69a3
hemadevi-nyros <40348452+hemadevi-nyros@users.noreply.github.com>
2019-05-08 09:59:00 +0530

    Aqua Deals

--- app/screens/Form.js
+++ app/screens/Form.js
@@ -5,40 +5,35 @@ import { Dimensions } from 'react-native';
 import { withNavigation } from 'react-navigation';
 import { Avatar, Badge, PricingCard,Button, Input} from 'react-native-elements';
 
-
 class Form extends Component{
-	render() {
-	  return(
-     <View style={styles.form_elements}>
-       <Image resizeMode="contain" source={require('./images/india.png')} style={styles.icon} />
-       <Text style={styles.text}>+91</Text>
-       <TextInput placeholder="Enter your mobile number" onChangeText={() => {
-            this.props.navigation.navigate('Signin');}} style={styles.inputtext} />
-     </View>
-	  );
-
-	};
+render() {
+return(
+ <View style={styles.form_elements}>
+  <Image resizeMode="contain" source={require('./images/india.png')} style={styles.icon} />
+  <Text style={styles.text}>+91</Text>
+  <TextInput placeholder="Enter your mobile number" onChangeText={() => {
+    this.props.navigation.navigate('Signin');}} style={styles.inputtext} />
+ </View>
+);
+};
 };
 
 const styles = StyleSheet.create({
-   
-   icon: {
-    width:30,
-    height: 30,
-    marginLeft: 20,
-    marginTop: 20,
-   },
-   text: {
-    marginTop: 20,
-   },
-   form_elements:{
-    flexDirection: 'row',
-   },
-   inputtext:{
-    marginLeft: 5,
-    marginTop:3,
-   }
+ icon: {
+ width:30,
+ height: 30,
+ marginLeft: 20,
+ marginTop: 20,
+ },
+ text: {
+ marginTop: 20,
+ },
+ form_elements:{
+ flexDirection: 'row',
+ },
+ inputtext:{
+ marginLeft: 5,
+ marginTop:3,
+ }
 });
-
-
 export default withNavigation(Form);
