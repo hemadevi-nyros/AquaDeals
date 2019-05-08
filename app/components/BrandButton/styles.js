73227c145670bd5242b7af202a3e7a1c4e927fce
hemadevi-nyros <40348452+hemadevi-nyros@users.noreply.github.com>
2019-05-08 09:35:48 +0530

    Aqua Deals

--- app/components/BrandButton/styles.js
+++ app/components/BrandButton/styles.js
@@ -1,14 +1,12 @@
 import EStyleSheet from 'react-native-extended-stylesheet';
 
-
 export default EStyleSheet.create({
-	element:{
-		flexDirection: 'row',
-	},
-	image:{
-	  
-	  width: 300,
-	  height: 200,
-	  marginLeft: 10,
-	},
+element:{
+ flexDirection: 'row',
+},
+image:{	  
+ width: 300,
+ height: 200,
+ marginLeft: 10,
+},
 });
