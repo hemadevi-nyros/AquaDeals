051d947204764b864ce94336ba6c99d03559e928
hemadevi-nyros <40348452+hemadevi-nyros@users.noreply.github.com>
2019-05-08 09:42:32 +0530

    Aqua Deals

--- app/components/Buttons/styles.js
+++ app/components/Buttons/styles.js
@@ -1,19 +1,19 @@
 import EStyleSheet from 'react-native-extended-stylesheet';
 
 export default EStyleSheet.create({
- view:{
-  alignItems: "center",
-  justifyContent: "center",
-	marginTop: 25,
-	marginLeft: 25,
-	marginRight: 10,
- },
- element:{
-  flexDirection: 'row',
-	flexWrap: "wrap",
- },
- image:{
-	 width:  50,
-	 height: 50,
- },
+view:{
+alignItems: "center",
+justifyContent: "center",
+marginTop: 25,
+marginLeft: 25,
+marginRight: 10,
+},
+element:{
+flexDirection: 'row',
+flexWrap: "wrap",
+},
+image:{
+width:  50,
+height: 50,
+},
 });
