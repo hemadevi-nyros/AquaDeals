bbacad278ae4c70db8ccf4d7e47b8fa904855deb
hemadevi-nyros <40348452+hemadevi-nyros@users.noreply.github.com>
2019-05-08 09:54:14 +0530

    Aqua Deals

--- app/screens/Cart.js
+++ app/screens/Cart.js
@@ -6,60 +6,56 @@ import Form from './Form';
 
 class Cart extends Component{
 	render() {
-	  return(
-      <View>
-	  	 <ImageBackground resizeMode="contain" source={{uri:'https://www.aquadeals.in/admin/assets//images/web_imgs/login_bg.png'}} style={styles.backgroundimage}>
-         <TouchableOpacity onPress={() => {
-            this.props.navigation.navigate('Home');}}>
-           <Image
-                 resizeMode="contain"
-                 source={require('./images/home.png')}
-                style = {styles.home}
-           />
-         </TouchableOpacity>
-       </ImageBackground>
-       <Text style={styles.text}>SIGN IN OR REGISTER</Text>
-       <Form />
-      </View>
-	  );
-
+	return(
+   <View>
+	   <ImageBackground resizeMode="contain" source={{uri:'https://www.aquadeals.in/admin/assets//images/web_imgs/login_bg.png'}} style={styles.backgroundimage}>
+      <TouchableOpacity onPress={() => {
+        this.props.navigation.navigate('Home');}}>
+        <Image
+          resizeMode="contain"
+          source={require('./images/home.png')}
+          style = {styles.home}
+         />
+       </TouchableOpacity>
+      </ImageBackground>
+      <Text style={styles.text}>SIGN IN OR REGISTER</Text>
+      <Form />
+    </View>
+	 );
 	};
 };
-
 const styles = StyleSheet.create({
-   background:{
-   	 height: 400,
-   	 width: 360,
-   	 backgroundColor:"#FF7F50",
-   },
-   cart:{
-   	marginTop:50,
-   	marginLeft:100,
-   	alignItems:"center", 
-   	justifyContent:"center",
-    width: 150,
-   	height: 150,
-   },
-   home:{
-   	marginLeft: 310,
-   	marginTop: 50,
-   	marginRight: 0,
-   	width:30,
-   	height: 30,
-   },
-   backgroundimage: {
-    width:360,
-    height:400,
-   },
-    text: {
-    fontSize: 13,
-    marginBottom: 5,
-    marginLeft: 10,
-   },
-   form_elements:{
-    flexDirection: 'row',
-   }
-
+ background:{
+ height: 400,
+ width: 360,
+ backgroundColor:"#FF7F50",
+ },
+ cart:{
+ marginTop:50,
+ marginLeft:100,
+ alignItems:"center", 
+ justifyContent:"center",
+ width: 150,
+ height: 150,
+ },
+ home:{
+ marginLeft: 310,
+ marginTop: 50,
+ marginRight: 0,
+ width:30,
+ height: 30,
+ },
+ backgroundimage: {
+ width:360,
+ height:400,
+ },
+ text: {
+ fontSize: 13,
+ marginBottom: 5,
+ marginLeft: 10,
+ },
+ form_elements:{
+ flexDirection: 'row',
+ }
 });
-
 export default withNavigation(Cart);
